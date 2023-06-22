e4a509e9f028ae5f67fdc4ea6433cf3b8d77b5b2
exic <ping.u@ya.ru>
2023-06-22 23:23:18 +0300

    intermediate commit

---
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -0,0 +1,13 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// BEGIN
+class App {
+    public static List<Home> buildApartmentsList(List<Home> list) {
+        return new ArrayList<>();
+    }
+}
+// END
